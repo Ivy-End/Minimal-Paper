8920dee85ac3a5bce6d3c6dc9de5907256a9c9bd
Ivy-End <admin@ivy-end.com>
2014-12-28 23:13:38 +0800

    page

--- content-page.php
+++ content-page.php
@@ -7,9 +7,9 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-	</header><!-- .entry-header -->
+	<!--<header class="entry-header">
+		<?php // the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+	</header>--><!-- .entry-header -->
 
 	<div class="entry-content">
 		<?php the_content(); ?>
