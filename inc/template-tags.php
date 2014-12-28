1f5e8ca5d7a2288511cd0fd283451cff4ab6658d
Ivy-End <admin@ivy-end.com>
2014-12-28 19:37:21 +0800

    review

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -22,11 +22,11 @@ function simple_paging_nav() {
 		<div class="nav-links">
 
 			<?php if ( get_next_posts_link() ) : ?>
-			<div class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', 'simple' ) ); ?></div>
+			<div class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> 较早文章', 'simple' ) ); ?></div>
 			<?php endif; ?>
 
 			<?php if ( get_previous_posts_link() ) : ?>
-			<div class="nav-next"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>', 'simple' ) ); ?></div>
+			<div class="nav-next"><?php previous_posts_link( __( '较新文章 <span class="meta-nav">&rarr;</span>', 'simple' ) ); ?></div>
 			<?php endif; ?>
 
 		</div><!-- .nav-links -->
