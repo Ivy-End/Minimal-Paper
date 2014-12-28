7d9cdd48ce903ac4db120362fefbddfdcc27989e
Ivy-End <admin@ivy-end.com>
2014-12-28 23:33:34 +0800

    responsive

--- header.php
+++ header.php
@@ -31,7 +31,7 @@
 				</div><!-- .site-branding -->
 
 				<nav id="site-navigation" class="main-navigation" role="navigation">
-					<button class="menu-toggle" aria-controls="menu" aria-expanded="false"><?php _e( 'Primary Menu', 'simple' ); ?></button>
+					<button class="menu-toggle" aria-controls="menu" aria-expanded="false">导航</button>
 					<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
 				</nav><!-- #site-navigation -->
 			</header><!-- #masthead -->
