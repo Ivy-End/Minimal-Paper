0598c7c9746576bd8e50b064247d5a219643ae75
Ivy-End <admin@ivy-end.com>
2014-12-31 16:59:55 +0800

    comment e-mail notify

--- functions.php
+++ functions.php
@@ -143,3 +143,32 @@ function simple_get_https_avatar($avatar) {
 	return $avatar;
 }
 add_filter('get_avatar', 'simple_get_https_avatar');
+
+/**
+* E-mail notify
+*/
+function comment_mail_notify($comment_id) {
+  $comment = get_comment($comment_id);
+  $parent_id = $comment->comment_parent ? $comment->comment_parent : '';
+  $spam_confirmed = $comment->comment_approved;
+  if (($parent_id != '') && ($spam_confirmed != 'spam')) {
+    $wp_email = 'no-reply@' . preg_replace('#^www\.#', '', strtolower($_SERVER['SERVER_NAME'])); //e-mail 发出点, no-reply 可改为可用的 e-mail.
+    $to = trim(get_comment($parent_id)->comment_author_email);
+    $subject = '您在 [' . get_option("blogname") . '] 的留言有了新回复';
+    $message = '
+    <div style="background-color:#eef2fa; border:1px solid #d8e3e8; color:#111; padding:0 15px; -moz-border-radius:5px; -webkit-border-radius:5px; -khtml-border-radius:5px;">
+      <p>' . trim(get_comment($parent_id)->comment_author) . ', 您好!</p>
+      <p>您曾在《' . get_the_title($comment->comment_post_ID) . '》的留言:<br />'
+       . trim(get_comment($parent_id)->comment_content) . '</p>
+      <p>' . trim($comment->comment_author) . ' 给您的回复:<br />'
+       . trim($comment->comment_content) . '<br /></p>
+      <p>您可以点击 查看回复完整內容</p>
+      <p>欢迎再度光临 ' . get_option('blogname') . '</p>
+      <p>(此邮件由系统自动发送，请勿回复.)</p>
+    </div>';
+    $from = "From: \"" . get_option('blogname') . "\" <$wp_email>";
+    $headers = "$from\nContent-Type: text/html; charset=" . get_option('blog_charset') . "\n";
+    wp_mail( $to, $subject, $message, $headers );
+  }
+}
+add_action('comment_post', 'comment_mail_notify');
